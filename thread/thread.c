2bd8e45c4614ca172123d479f64e8ae0db4c553a
Jieun <yjeka@ajou.ac.kr>
2020-08-12 21:58:08 +0900

    Add thread practice

---
+++ thread/thread.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+short int scores[30]={0};
+
+void * sthread1(void *arg)
+{
+	int sum=0;
+    float avg;
+	for(int i=0; i<=(int)arg; i++) {
+		sum += scores[i];
+	}
+    avg = sum / 30.0;
+    printf("합 : %d, 평균 : %f\n", sum, avg);
+	pthread_exit(arg);
+}
+
+void * sthread2(void *arg)
+{
+    int key, i, j;
+	for(i=1;i<(int)arg;i++)
+	{
+		key = scores[i];
+		for(j=i-1;j>=0 && scores[j]<key;j--)
+		{
+			scores[j+1] = scores[j];
+		}
+		scores[j+1] = key;
+	}
+
+    for(i=0;i<(int)arg;i++){
+        printf("%2d등 : %d\n", i+1, scores[i]);
+    }
+	pthread_exit(arg);
+}
+
+int main(void)
+{
+	pthread_t tid1, tid2;
+	int status1, status2;
+	pthread_attr_t attr;
+	
+    for(int i = 0;i<30;i++)
+    {
+        scanf("%hd", &scores[i]);
+    }
+
+    printf("성적 sample\n");
+    for(int i = 0;i<30;i++)
+    {
+        printf("%3hd\n", scores[i]);
+    }
+
+    printf("Main start!\n");
+	status1 = pthread_create(&tid1, NULL, sthread1, (void*)30);
+	if(status1 != 0) {
+		perror("thread1 create");
+		exit(1);
+	}
+
+    status2 = pthread_create(&tid2, NULL, sthread2, (void*)30);
+    if(status2 != 0) {
+		perror("thread2 create");
+		exit(1);
+	}	
+    printf("Main end!\n");
+
+	pthread_exit(NULL);
+    return 0;
+}
